aa52a246349c95d803c783c2c370eb7a0b4ad6a3
your new username <serviceweb@thsbo.it>
2024-12-09 14:05:35 +0100

    a

--- src/app/servizi/base.service.ts
+++ src/app/servizi/base.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BaseService {
-  baseUrl: string = 'http://192.168.1.177:8585/api';
+  baseUrl: string = 'https://thsbologna.it:8585/api';
 
   constructor() { }
 }
